8acd6d5c4ddf4109cc77ef540150f3fa3268340e
urislam4 <isihohka@gmail.com>
2022-09-11 16:23:13 +0600

    js-part

---
+++ border.js
@@ -0,0 +1,13 @@
+const text = document.querySelector('h1');
+
+text.onmouseenter = () =>{
+    console.log("мышь в тексте");
+}
+
+text.onmouseleave = () =>{
+    console.log("мышь покинул  console")
+}
+
+
+
+
